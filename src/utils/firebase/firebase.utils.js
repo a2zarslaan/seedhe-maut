4de4b2906ebf39707480ee3abb4d1bbb1ec8fec7
Amar Arslaan <a2zarslaan@hotmail.com>
2022-08-15 18:38:30 +0100

    feat: create user in db after auth

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+
 const firebaseConfig = {
   apiKey: "AIzaSyDz8RNIvb3pZ47pienD9VthudeXRHkqEOg",
   authDomain: "seedhe-maut-db.firebaseapp.com",
@@ -22,3 +24,30 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    const userSnapshot = await getDoc(userDocRef);
+
+    //if userSnapshot doesn't exist, create one, else return userDocRef
+    if(!userSnapshot.exists()) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        //set the document
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        }
+        catch(error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userDocRef;
+}
