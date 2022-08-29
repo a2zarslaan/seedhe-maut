cd31b88627960b890651b7268f60af394437b7ec
Amar Arslaan <a2zarslaan@hotmail.com>
2022-08-29 14:01:42 +0100

    feat: cart dropdown component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,9 @@
 import { Fragment, useContext } from "react"
 import { Link, Outlet } from "react-router-dom"
+
+import CartIcon from "../../components/cart-icon/cart-icon.component"
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component"
+
 import Logo from '../../assets/logo.png' 
 import { UserContext } from "../../contexts/user.context"
 import { signOutUser } from "../../utils/firebase/firebase.utils"
@@ -21,7 +25,9 @@ return (
                     currentUser ? (<span className="nav-link" onClick={signOutUser}>Sign Out</span>) : 
                     (<Link className="nav-link" to='/auth'>Sign In</Link>)
                 }
+                <CartIcon />
             </div>
+            <CartDropdown />
         </div>
         <Outlet />
     </Fragment>
