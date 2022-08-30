5240713c9466aa65d9ac222e7ac1d02f44bfe597
Amar Arslaan <a2zarslaan@hotmail.com>
2022-08-30 20:34:50 +0100

    feat: cart toggle display

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,32 +6,34 @@ import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component
 
 import Logo from '../../assets/logo.png' 
 import { UserContext } from "../../contexts/user.context"
+import { CartContext } from "../../contexts/cart.context"
 import { signOutUser } from "../../utils/firebase/firebase.utils"
 
 import './navigation.styles.scss'
 
 const Navigation = () => {
     const {currentUser} = useContext(UserContext);
+    const {isCartOpen} = useContext(CartContext);
 
-return (
-    <Fragment>
-        <div className="navigation">
-            <Link className="logo-contianer" to='/'>
-                <img src={Logo} alt='logo' className='logo' />
-            </Link>
-            <div className="nav-links-container">
-                <Link className="nav-link" to='/shop'>Shop</Link>
-                {
-                    currentUser ? (<span className="nav-link" onClick={signOutUser}>Sign Out</span>) : 
-                    (<Link className="nav-link" to='/auth'>Sign In</Link>)
-                }
-                <CartIcon />
+    return (
+        <Fragment>
+            <div className="navigation">
+                <Link className="logo-contianer" to='/'>
+                    <img src={Logo} alt='logo' className='logo' />
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='/shop'>Shop</Link>
+                    {
+                        currentUser ? (<span className="nav-link" onClick={signOutUser}>Sign Out</span>) : 
+                        (<Link className="nav-link" to='/auth'>Sign In</Link>)
+                    }
+                    <CartIcon/>
+                </div>
+                {isCartOpen && <CartDropdown />}
             </div>
-            <CartDropdown />
-        </div>
-        <Outlet />
-    </Fragment>
-)
+            <Outlet />
+        </Fragment>
+    )
 }
 
 export default Navigation
