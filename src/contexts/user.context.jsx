62b2097ce11d82a566b8f77f4c5a81020557557d
Amar Arslaan <a2zarslaan@hotmail.com>
2022-10-11 17:08:03 +0100

    refactor: remove console log

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -2,6 +2,8 @@ import { createContext, useReducer, useEffect } from "react";
 
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
 
+import {createAction} from'../utils/reducer/reducer.utils'
+
 //actual value we want to access
 export const UserContext = createContext({
     currentUser: null,
@@ -12,9 +14,11 @@ export const USER_ACTION_TYPES = {
     SET_CURRENT_USER: 'SET_CURRENT_USER',
 }
 
+const INITIAL_STATE = {
+    currentUser: null,
+}
+
 const userReducer = (state, action) => {
-    console.log('dispatched');
-    console.log(action);
     const {type, payload} = action;
     
     switch(type) {
@@ -28,21 +32,13 @@ const userReducer = (state, action) => {
     }
 }
 
-const INITIAL_STATE = {
-    currentUser: null,
-}
-
 export const UserProvider = ({children}) => {
-    const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
-    const {currentUser} = state;
-
-    console.log(currentUser);
-
+    const [{currentUser}, dispatch] = useReducer(userReducer, INITIAL_STATE);
+    console.log(currentUser)
     const setCurrentUser = (user) => {
-        dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user});
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
     }
-
-    const value = {currentUser, setCurrentUser};
+    
     useEffect(() => {
         const unsubscribe = onAuthStateChangedListener((user) => {
             if(user) {
@@ -50,9 +46,12 @@ export const UserProvider = ({children}) => {
             }
             setCurrentUser(user);
         });
-
+        
         //clean up fn
         return unsubscribe;
     },[]);
+
+    const value = {currentUser, setCurrentUser};
+
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
