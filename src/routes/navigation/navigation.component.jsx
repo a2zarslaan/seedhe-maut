1d34c1cc2abdf38328991a6f909d107413abab90
a2zarslaan <a2zarslaan@hotmail.com>
2022-11-16 15:07:19 +0000

    feat: redux store

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,39 +1,38 @@
-import { Fragment, useContext } from "react"
-import { Link, Outlet } from "react-router-dom"
+import { useContext } from 'react';
+import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-import CartIcon from "../../components/cart-icon/cart-icon.component"
-import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component"
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
-import Logo from '../../assets/logo.png' 
-import { UserContext } from "../../contexts/user.context"
-import { CartContext } from "../../contexts/cart.context"
-import { signOutUser } from "../../utils/firebase/firebase.utils"
+import Logo from '../../assets/logo.png';
+import { CartContext } from '../../contexts/cart.context';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
 
-import {NavigationContainer, LogoContainer, LogoImg, NavLinks, NavLink} from './navigation.styles'
+import {
+  NavigationContainer, LogoContainer, LogoImg, NavLinks, NavLink,
+} from './navigation.styles';
 
-const Navigation = () => {
-    const {currentUser} = useContext(UserContext);
-    const {isCartOpen} = useContext(CartContext);
+function Navigation() {
+  const currentUser = useSelector((state) => state.user.currentUser);
+  const { isCartOpen } = useContext(CartContext);
 
-    return (
-        <Fragment>
-            <NavigationContainer>
-                <LogoContainer to='/'>
-                    <LogoImg src={Logo} alt='logo' />
-                </LogoContainer>
-                <NavLinks>
-                    <NavLink to='/shop'>Shop</NavLink>
-                    {
-                        currentUser ? (<NavLink as='span' onClick={signOutUser}>Sign Out</NavLink>) : 
-                        (<NavLink className="nav-link" to='/auth'>Sign In</NavLink>)
-                    }
-                    <CartIcon/>
-                </NavLinks>
-                {isCartOpen && <CartDropdown />}
-            </NavigationContainer>
-            <Outlet />
-        </Fragment>
-    )
+  return (
+    <>
+      <NavigationContainer>
+        <LogoContainer to="/">
+          <LogoImg src={Logo} alt="logo" />
+        </LogoContainer>
+        <NavLinks>
+          <NavLink to="/shop">Shop</NavLink>
+          {currentUser ? (<NavLink as="span" onClick={signOutUser}>Sign Out</NavLink>) : (<NavLink className="nav-link" to="/auth">Sign In</NavLink>)}
+          <CartIcon />
+        </NavLinks>
+        {isCartOpen && <CartDropdown />}
+      </NavigationContainer>
+      <Outlet />
+    </>
+  );
 }
 
-export default Navigation
+export default Navigation;
