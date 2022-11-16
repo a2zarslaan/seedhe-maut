f44a0ed911277508bd64a9782c16d5e6daa3d889
a2zarslaan <a2zarslaan@hotmail.com>
2022-11-16 15:09:48 +0000

    feat: user slice reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,57 +1,15 @@
-import { createContext, useReducer, useEffect } from "react";
+import { createContext } from 'react';
 
-import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
-
-import {createAction} from'../utils/reducer/reducer.utils'
-
-//actual value we want to access
+// actual value we want to access
 export const UserContext = createContext({
-    currentUser: null,
-    setCurrentUser: () => null
-})
+  currentUser: null,
+  setCurrentUser: () => null,
+});
 
-export const USER_ACTION_TYPES = {
-    SET_CURRENT_USER: 'SET_CURRENT_USER',
-}
+// export function UserProvider({ children }) {
+//   const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
 
-const INITIAL_STATE = {
-    currentUser: null,
-}
+//   const value = { currentUser, setCurrentUser };
 
-const userReducer = (state, action) => {
-    const {type, payload} = action;
-    
-    switch(type) {
-        case USER_ACTION_TYPES.SET_CURRENT_USER:
-            return {
-                ...state, 
-                currentUser: payload,
-            };
-        default:
-            throw new Error(`Unhandled type ${type} in userReducer`);
-    }
-}
-
-export const UserProvider = ({children}) => {
-    const [{currentUser}, dispatch] = useReducer(userReducer, INITIAL_STATE);
-
-    const setCurrentUser = (user) => {
-        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
-    }
-    
-    useEffect(() => {
-        const unsubscribe = onAuthStateChangedListener((user) => {
-            if(user) {
-                createUserDocumentFromAuth(user);
-            }
-            setCurrentUser(user);
-        });
-        
-        //clean up fn
-        return unsubscribe;
-    },[]);
-
-    const value = {currentUser, setCurrentUser};
-
-    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
-}
+//   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+// }
