a22deff8558fbb1b6db4438e877feb5229699f3d
a2zarslaan <a2zarslaan@hotmail.com>
2022-11-16 15:05:16 +0000

    style: eslint formatting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
-import Shop from "./routes/shop/shop.component"
-import Checkout from "./routes/checkout/checkout.component";
-import Authentication from "./routes/authentication/authentication.component";
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
-const App = () => {
+import { Routes, Route } from 'react-router-dom';
+
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from './utils/firebase/firebase.utils';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import Shop from './routes/shop/shop.component';
+import Checkout from './routes/checkout/checkout.component';
+import Authentication from './routes/authentication/authentication.component';
+import { setCurrentUser } from './store/user/user.action';
+
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+
+    // clean up fn
+    return unsubscribe;
+  }, []);
+
   return (
-  <Routes>
-    <Route path='/' element={<Navigation/>}>
-      <Route index={true} element={<Home/>} />
-      <Route path='shop/*' element={<Shop/>} />
-      <Route path='auth' element={<Authentication/>} />
-      <Route path='checkout' element={<Checkout/>} />
-    </Route>
-  </Routes>
-  )
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop/*" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,16 +1,21 @@
+/* eslint-disable */
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth'
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged,
+} from 'firebase/auth';
 
-import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs } from 'firebase/firestore'
+import {
+  getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs,
+} from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDz8RNIvb3pZ47pienD9VthudeXRHkqEOg",
-  authDomain: "seedhe-maut-db.firebaseapp.com",
-  projectId: "seedhe-maut-db",
-  storageBucket: "seedhe-maut-db.appspot.com",
-  messagingSenderId: "477441356732",
-  appId: "1:477441356732:web:763d092ba55ffbf934b1b9"
+  apiKey: 'AIzaSyDz8RNIvb3pZ47pienD9VthudeXRHkqEOg',
+  authDomain: 'seedhe-maut-db.firebaseapp.com',
+  projectId: 'seedhe-maut-db',
+  storageBucket: 'seedhe-maut-db.appspot.com',
+  messagingSenderId: '477441356732',
+  appId: '1:477441356732:web:763d092ba55ffbf934b1b9',
 };
 
 // Initialize Firebase
@@ -19,8 +24,8 @@ const firebaseApp = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
-    prompt: 'select_account'
-})
+  prompt: 'select_account',
+});
 
 // an app has only a single auth, it helps stay upto date on the authentication states happening throughout the app
 export const auth = getAuth();
@@ -30,71 +35,70 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 export const db = getFirestore();
 
 export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
-    const collectionRef = collection(db, collectionKey);
-    const batch = writeBatch(db);
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
 
-    objectsToAdd.forEach((object) => {
-        const docRef = doc(collectionRef, object.title.toLowerCase());
-        batch.set(docRef, object);
-    });
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
 
-    await batch.commit();
-}
+  await batch.commit();
+};
 
 export const getCategoriesAndDocuments = async () => {
-    const collectionRef = collection(db, 'categories');
-    const q = query(collectionRef);
+  const collectionRef = collection(db, 'categories');
+  const q = query(collectionRef);
 
-    const querySnapshot = await getDocs(q);
-    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
-        const {title,items} = docSnapshot.data();
-        acc[title.toLowerCase()] = items;
-        return acc;
-    }, {});
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
 
-    return categoryMap;
-}
+  return categoryMap;
+};
 
 export const createUserDocumentFromAuth = async (userAuth) => {
-    if(!userAuth) return;
+  if (!userAuth) return;
 
-    const userDocRef = doc(db, 'users', userAuth.uid);
-    const userSnapshot = await getDoc(userDocRef);
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
 
-    //if userSnapshot doesn't exist, create one in db, else return userDocRef
-    if(!userSnapshot.exists()) {
-        const {displayName, email} = userAuth;
-        const createdAt = new Date();
+  // if userSnapshot doesn't exist, create one in db, else return userDocRef
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
 
-        //set the document
-        try {
-            await setDoc(userDocRef, {
-                displayName,
-                email,
-                createdAt
-            })
-        }
-        catch(error) {
-            console.log('error creating user', error.message);
-        }
+    // set the document
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
     }
+  }
 
-    return userDocRef;
-}
+  return userDocRef;
+};
 
-//email and pass login doesn't need a provider because these are native sign-in methods with firebase
+// email and pass login doesn't need a provider because these are native sign-in methods with firebase
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
-    if(!email || !password) return;
+  if (!email || !password) return;
 
-    return await createUserWithEmailAndPassword(auth, email, password)
-}
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
 export const signInAuthUserWithEmailAndPassword = async (email, password) => {
-    if(!email || !password) return;
+  if (!email || !password) return;
 
-    return await signInWithEmailAndPassword(auth, email, password)
-}
+  return await signInWithEmailAndPassword(auth, email, password);
+};
 
 export const signOutUser = async () => await signOut(auth);
 
-//part of the observer pattern, this listener always listens for changes in auth, making it easier to handle auth events
+// part of the observer pattern, this listener always listens for changes in auth, making it easier to handle auth events
 export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
